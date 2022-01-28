fda46a1e16c5e784432493255e27aa3f23c85a04
Aiden <fsd76005588@gmail.com>
2022-01-28 16:29:39 +0900

    [ADD] boj.2164-카드2

--- boj/2164.카드2.py
+++ boj/2164.카드2.py
@@ -2,10 +2,10 @@ import sys
 from collections import deque
 
 # input_number = int(sys.stdin.readline())
-input_number = [1,2,3,4,5,6]
+input_number = [1, 2, 3, 4, 5, 6]
 
 input_array = deque()
-for i in range(input_number):
+for i in range(len(input_number)):
     input_array.append(i + 1)
 
 while len(input_array) > 1:
